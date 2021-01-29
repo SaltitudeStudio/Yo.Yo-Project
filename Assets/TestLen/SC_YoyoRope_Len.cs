2a6e5e67a3c4b11737d532c14771f6168a405f9e
azymutt <azymutt@users.noreply.github.com>
2021-01-29 02:05:22 +0100

    lienrender et compte rendu de tests

--- Assets/TestLen/SC_YoyoRope_Len.cs
+++ Assets/TestLen/SC_YoyoRope_Len.cs
@@ -23,7 +23,14 @@ public class SC_YoyoRope_Len : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //              /!\
 
+        // J'ai ajouté un line render vite fait, mais j'ai surtout testé le distance joint qui fait nimp du coup, j'ai l'impression qu'il transmet pas la force dans tous
+        // les cas où on déplace la position autrement qu'en physique => ^super gênant, le spring joint était prometteur au niveau du réalisme, mais l'elasticité est trop dure à controler. 
+        // Le hinge joint me semble être le plus prometteur en l'état, il a un comportement assez correct et s'ajuste plus ou moins bien, à voir.
+        //Cy
+
+        UpdateLineRenderer();
     }
 
     public void AddRopeSegment()
@@ -46,6 +53,9 @@ public class SC_YoyoRope_Len : MonoBehaviour
         }
 
         Rigidbody2D _curSegmentRb = _newSegment.AddComponent<Rigidbody2D>();
+        _curSegmentRb.mass = 0.1f;
+
+
         DistanceJoint2D _curSegmentJoint = _newSegment.AddComponent<DistanceJoint2D>();
 
         _curSegmentJoint.autoConfigureDistance = false;
@@ -60,6 +70,20 @@ public class SC_YoyoRope_Len : MonoBehaviour
     public void RemoveRopeSegment()
     {
         Debug.Log("RemoveRopeSegment");
+    }
+
+
+    public void UpdateLineRenderer()
+    {
+        LineRenderer lineRender = GetComponent<LineRenderer>();
+
+        lineRender.positionCount = ropeSegments.Count;
+
+        for (int i = 0; i < ropeSegments.Count; i++)
+        {
+            lineRender.SetPosition(i, ropeSegments[i].transform.position);
+        }
+
     }
 
 }
